0c901651fc8cba194e85082eb76540cc6ee15b97
jhsjs1271 <47516242-jhsjs1271@users.noreply.replit.com>
2025-09-12 13:26:20 +0000

    update

--- .ccls-cache/@home@runner@workspace/src@version.h
+++ .ccls-cache/@home@runner@workspace/src@version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define RES_VERSION_HEX      0x010000 /* 01.00.00 */
-#define RES_VERSION_STRING   "1.0.0"
-#define RES_VERSION_STRING4  "1.00"
+#define RES_VERSION_STRING   "1.0.1"
+#define RES_VERSION_STRING4  "1.01"
 #define RES_VERSION_DATE     "Sep 06th 2025"
 #define RES_VERSION_DATE_ISO "2025-09-06"
 #define RES_VERSION_YEAR     "2025"
